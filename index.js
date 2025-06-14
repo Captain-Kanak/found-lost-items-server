cb2610776a71a630862086386f1bf388f300f90f
Captain-Kanak <kanakroy835@gmail.com>
2025-06-14 13:33:14 +0600

    delete item done

--- index.js
+++ index.js
@@ -75,6 +75,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/items/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await itemsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // items related APIs
     app.get("/recovered-items", async (req, res) => {
       const result = await recoveredItemsCollection
