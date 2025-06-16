8b738985d0db43f0b48f944fa52275d58ad0ae33
Captain-Kanak <kanakroy835@gmail.com>
2025-06-17 01:25:46 +0600

    set production origin

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://find-lost-items-993d8.web.app"],
     credentials: true,
   })
 );
